ef05a2f77b4877857b1cd83c0a7ca1d01f80bc82
Kitiya <kate.kitiya@gmail.com>
2020-06-27 18:01:38 -0400

    162. The products overview screen, Setup redux store

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+
+const ProductsOverviewScreen = (props) => {
+  return "";
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
